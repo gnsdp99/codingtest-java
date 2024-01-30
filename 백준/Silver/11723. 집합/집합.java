2b79dc3dc535472b44ec2c7d8e15e4650390c277
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2024-01-30 22:33:38 +0900

    [Silver V] Title: 집합, Time: 976 ms, Memory: 324572 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author 김예훈
+ * @date 24/01/30
+ * @link https://www.acmicpc.net/problem/11723
+ * @keyword_solution
+ * 이 문제를 배열이나 컬렉션으로 구현하면 연산 횟수가 너무 많아진다.
+ * 물론 x를 인덱스로 하는 boolean 배열을 사용할 수 있지만 20B를 사용해야 한다.
+ * 또한 all과 empty 연산 시에는 배열 전체를 탐색해야 한다.
+ * 하지만 비트마스킹을 사용하면 모든 연산을 한 번의 비트 연산으로 끝낼 수 있다.
+ * 또한 x는 [1, 20]이므로 int 변수 하나로 해결할 수 있다.
+ *
+ * 초기화: 비어있는 공집합이므로 0.
+ * add 연산: x번째 비트만 1인 수와 OR 연산을 한다. (1 << x)
+ * remove 연산: x번째 비트만 1인 수와 AND 연산 후 존재하면 XOR 연산한다.
+ * check 연산: x번째 비트만 1인 수와 AND 연산을 하고 그 결과가 0이 아닌지 확인한다.
+ * toggle 연산: x번째 비트만 1인 수와 XOR 연산을 한다.
+ * all 연산: 모든 비트가 1인 수와 OR 연산을 한다. (그냥 (1 << 21) - 1)
+ * empty 연산: 0과 AND 연산을 한다. (그냥 0)
+ * @input
+ * 연산의 수는 1 <= M <= 3_000_000이다.
+ * 모든 연산이 배열 전체를 탐색하는 것이라 해도 시간은 충분하지만, 비트마스킹을 이용하면 충분히 시간을 줄일 수 있다.
+ * @output
+ * check연산을 수행할 때만 그 결과를 출력한다.
+ * @time_complex O(M)
+ * @perf
+ */
+
+public class Main {
+
+    static int bitSet = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int M = Integer.parseInt(br.readLine());
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            String op = st.nextToken();
+            if (op.equals("all")) {
+                bitSet = (1 << 21) - 1;
+                continue;
+            } else if (op.equals("empty")) {
+                bitSet = 0;
+                continue;
+            }
+
+            int num = Integer.parseInt(st.nextToken());
+            if (op.equals("add")) {
+                bitSet = bitSet | (1 << num);
+            } else if (op.equals("remove")) {
+                if (check(num)) {
+                    toggle(num);
+                }
+            } else if (op.equals("check")) {
+                if (check(num)) {
+                    sb.append("1");
+                } else {
+                    sb.append("0");
+                }
+                sb.append("\n");
+            } else if (op.equals("toggle")) {
+                toggle(num);
+            }
+        }
+        System.out.println(sb);
+    }
+
+    static boolean check(int x) {
+        return (bitSet & (1 << x)) != 0;
+    }
+
+    static void toggle(int x) {
+        bitSet = bitSet ^ (1 << x);
+    }
+}
