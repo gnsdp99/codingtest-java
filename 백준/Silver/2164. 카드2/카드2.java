fcb3d9ad2f33dbedc1c6fb865026a8620c4091de
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2024-02-02 13:51:40 +0900

    [Silver IV] Title: 카드2, Time: 116 ms, Memory: 17876 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -13,12 +13,16 @@ import java.util.Queue;
  * 1. 제일 위에 있는 카드를 버린다.
  * 2. 제일 위에 있는 카드를 제일 아래로 옮긴다.
  * >> 큐
+ 
+ * - 1, 3, ..., 2n-1은 무조건 버려짐.
+ * - N이 홀수이면, 한 바퀴 돌고 난 후 4, ..., N - 1, 2 가 됨.
+ * - N이 짝수이면, 한 바퀴 돌고 난 후 2, ..., N이 됨.  
  * @input
  * N [1, 500_000] 
  * @output   
  * 남는 카드 번호 출력.
  * @time_complex O(N)
- * @perf 
+ * @perf 23,848kb, 120ms
  */
 
 public class Main {
@@ -28,11 +32,19 @@ public class Main {
 		
 		int N = Integer.parseInt(br.readLine());
 		Queue<Integer> queue = new ArrayDeque<>();
-		for (int i = 1; i <= N; i++) {
-			queue.offer(i);
+		
+		if (N == 1) {
+			System.out.println(1);
+			return;
 		}
 		
-		int size = N;
+		int start = N % 2 == 0 ? 2 : 4;
+		for (int i = start; i <= N; i+=2) {				
+			queue.offer(i);
+		}
+		if (N % 2 == 1) queue.offer(2);
+		
+		int size = queue.size();
 		while (size >= 2) {
 			queue.poll();
 			queue.offer(queue.poll());
