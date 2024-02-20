6d4598d79ee811c81548f1ee21f8e6d74f837335
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2024-02-20 17:01:08 +0900

    [Gold IV] Title: 치즈, Time: 100 ms, Memory: 12164 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -6,11 +6,10 @@
  * 구현
  * flood fill 알고리즘
  * - (0, 0)은 항상 공기이므로 여기서부터 탐색을 시작한다.
- * - 각 공기에서 4방으로 인접한 치즈는 녹았음을 표시한다.
+ * - 각 공기에서 4방으로 인접한 치즈는 공기로 만든다.
  * - 치즈는 더 이상 탐색 대상에 넣지 않음으로써 가장자리만 녹인다.
  * - 공기는 계속해서 탐색 대상에 추가한다.
- * - 또한 탐색 완료한 공기 탐색 완료를 표시하고 다음 번에 탐색하지 않도록 한다.
- * - 탐색을 할 때마다 녹은 치즈의 개수를 리턴하면 마지막 탐색에서 마지막 치즈의 개수를 구할 수 있다. 
+ * - 치즈를 큐에 넣지 않으면서 다음 탐색의 대상이 되도록 하기 위해 공기와 치즈의 방문 여부를 따로 관리한다.
  *  
  * @input
  * - 판의 크기 N, M [1, 100]
@@ -21,7 +20,7 @@
  * - 모두 녹기 한 시간 전 남아있는 치즈조각의 개수 출력
  * 
  * @time_complex  O(N^2)
- * @perf 
+ * @perf 13,004kb, 116ms
  */
 import java.io.IOException;
 import java.io.BufferedReader;
@@ -41,6 +40,8 @@ public class Main {
 
 	static int N, M, ansTime, ansNum;
 	static int[][] board;
+	static boolean[][] visited;
+	static Queue<Pos> cheeseQueue = new ArrayDeque<>();
 	static Pos[] delta = {new Pos(-1, 0), new Pos(0, 1), new Pos(1, 0), new Pos(0, -1)};
 	
 	public static void main(String[] args) throws IOException {
@@ -52,50 +53,64 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		board = new int[N][M];
-		int totalCheese = 0;
+		visited = new boolean[N][M];
+		
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				board[i][j] = Integer.parseInt(st.nextToken());
-				if (board[i][j] == 1) totalCheese++;
 			}
 		}
 
-		while (totalCheese > 0) {
-			int numCheese = bfsFloodFill(0, 0);
-			ansNum = numCheese;
-			totalCheese -= numCheese;
-			ansTime++;	
+		bfsFloodFill(0, 0); // 0인 곳은 flood fill
+	
+		while (!cheeseQueue.isEmpty()) {
+			ansTime++;
+			int size = cheeseQueue.size();
+			ansNum = size;
+			for (; size > 0; size--) { // 녹은 치즈들의 4방 탐색
+				Pos pos = cheeseQueue.poll();
+				board[pos.x][pos.y] = 2; // 공기의 방문 처리
+				
+				for (int d = 0; d < delta.length; d++) {
+					int nx = pos.x + delta[d].x;
+					int ny = pos.y + delta[d].y;
+					if (isIn(nx, ny)) {
+						if (board[nx][ny] == 0) bfsFloodFill(nx, ny); // 0인 곳은 flood fill
+						else if (!visited[nx][ny] && board[nx][ny] == 1) {
+							visited[nx][ny] = true;
+							cheeseQueue.offer(new Pos(nx, ny)); // 안쪽 치즈 추가
+						}
+					}
+				}
+			}
 		}
 		sb.append(ansTime).append("\n").append(ansNum);
 		System.out.println(sb);
 	}
 	
-	static int bfsFloodFill(int sx, int sy) {
+	static void bfsFloodFill(int sx, int sy) {
 		Queue<Pos> queue = new ArrayDeque<>();
-		boolean[][] visited = new boolean[N][M];
 		queue.offer(new Pos(sx, sy));
-		visited[sx][sy] = true;
-		int numCheese = 0;
+		board[sx][sy] = 2;
 		
 		while(!queue.isEmpty()) {
 			Pos cur = queue.poll();
 			for (int d = 0; d < delta.length; d++) {
 				int nx = cur.x + delta[d].x;
 				int ny = cur.y + delta[d].y;
-				if (isIn(nx, ny) && !visited[nx][ny]) {
-					visited[nx][ny] = true;
+				if (isIn(nx, ny)) {
 					if (board[nx][ny] == 0) {
 						queue.offer(new Pos(nx, ny));
+						board[nx][ny] = 2;
 					}
-					else if (board[nx][ny] == 1) {
-						numCheese++;
-						board[nx][ny] = 0;
+					else if (!visited[nx][ny] && board[nx][ny] == 1) {
+						visited[nx][ny] = true;
+						cheeseQueue.offer(new Pos(nx, ny));
 					}
 				}
 			}
 		}
-		return numCheese;
 	}
 	
 	static boolean isIn(int x, int y) {
