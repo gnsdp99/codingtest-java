e1df2faf6a96ae6f88e8d609712cdb5e524e0a7a
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2024-02-28 14:48:00 +0900

    [Silver V] Title: 다리 놓기, Time: 80 ms, Memory: 11832 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -8,13 +8,10 @@ import java.util.StringTokenizer;
  * @date 24/02/28
  * @link https://www.acmicpc.net/problem/1010
  * @keyword_solution  
- * - 완전탐색 시 30!이므로 안됨.
- * - 서쪽의 i번 사이트는 동쪽의 [i, M - N + i] 까지만 연결할 수 있음
- * - D(i, j): 서쪽의 i번 사이트를 동쪽의 j번 사이트에 연결하는 경우의 수
- * - D[i][j] = D[i][j - 1] + D[i - 1][j - 1] (j <= M - N + i)
- * - D[i][i] = 1
- * - D[i][j] = 0 (j < i)
- * - D[i][j] = 0 (j > M - N + i)
+ * - M개의 동쪽 사이트 중에서 N개의 사이트를 조합하는 문제임.
+ * - 앞에서 선택한 사이트 아래로만 선택할 수 있기 때문에 조합임.
+ * - 하지만 입력 크기 때문에 완전탐색은 불가능.
+ * - 따라서 DP 사용.
  * 
  * @input 
  * - 서쪽 사이트 수 N [0, 30]
@@ -24,12 +21,13 @@ import java.util.StringTokenizer;
  * - 주어진 조건으로 다리를 지을 수 있는 경우의 수 출력
  *    
  * @time_complex O(NM)  
- * @perf 
+ * @perf
  */
 
 public class Main {
 
 	static int N, M;
+	static int[][] dp = new int[31][31];
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -42,18 +40,14 @@ public class Main {
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			
-			int[][] dp = new int[N + 1][M + 1];
-			for (int j = 1; j <= M - N + 1; j++) {
-				dp[1][j] = j;
-			}
-			for (int i = 2; i <= N; i++) {
-				dp[i][i] = 1;
-				for (int j = i + 1; j <= M - N + i; j++) {
-					dp[i][j] = dp[i][j - 1] + dp[i - 1][j - 1];
-				}
-			}
-			sb.append(dp[N][M]).append("\n");
+			sb.append(getCombination(M, N)).append("\n");
 		}
 		System.out.println(sb);
+	}
+	
+	static int getCombination(int n, int k) {
+		if (dp[n][k] > 0) return dp[n][k];
+		
+		return dp[n][k] = (k == 0 || n == k) ? 1 : getCombination(n - 1, k) + getCombination(n - 1, k - 1);
 	}
 }
