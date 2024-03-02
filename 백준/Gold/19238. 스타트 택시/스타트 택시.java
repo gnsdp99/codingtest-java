2156ab4d8568ab137e2ee7f340f8efa873fb798e
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2024-03-02 15:59:10 +0900

    [Gold II] Title: 스타트 택시, Time: 264 ms, Memory: 22480 KB -BaekjoonHub

---
+++ 백준/Gold/19238. 스타트 택시/스타트 택시.java
@@ -0,0 +1,197 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static class Pos {
+        int x, y;
+
+        Pos(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    static class Taxi {
+        Pos pos;
+        int fuel;
+        Taxi(int x, int y, int fuel) {
+            pos = new Pos(x, y);
+            this.fuel = fuel;
+        }
+        void setPos(int x, int y) {
+            this.pos.x = x;
+            this.pos.y = y;
+        }
+        void useFuel(int dist) {
+            fuel -= dist;
+        }
+        void chargeFuel(int dist) {
+            // 이동 거리 두 배만큼 충전
+            fuel += dist * 2;
+        }
+        int[] findPassenger() {
+            // 최단 거리 -> 작은 행 -> 작은 열의 승객을 찾는다.
+            int minDist = MAX_DIST;
+            int minRow = MAX_ROW;
+            int minCol = MAX_COL;
+            Queue<Pos> queue = new ArrayDeque<>();
+            int[][] visited = new int[N + 1][N + 1]; // 택시 위치로부터의 거리를 의미
+            for (int i = 1; i <= N; i++) {
+                Arrays.fill(visited[i], -1);
+            }
+            queue.offer(pos);
+            visited[pos.x][pos.y] = 0;
+
+            while (!queue.isEmpty()) {
+                Pos cur = queue.poll();
+                int x = cur.x;
+                int y = cur.y;
+                int key = N * x + y;
+                if (visited[x][y] > fuel) break; // 더 이상 탐색할 필요 없음
+
+                if (passengers.get(key) != null) {
+                    // 승객 찾음
+                    int dist = visited[x][y];
+                    if (dist < minDist
+                    || (dist == minDist && x < minRow)
+                    || (dist == minDist && x == minRow && y < minCol)) {
+                        minDist = dist;
+                        minRow = x;
+                        minCol = y;
+                    }
+                    if (dist == 0) break; // 가장 최단 거리를 찾음
+                }
+
+                for (int d = 0; d < deltaX.length; d++) {
+                    int nx = x + deltaX[d];
+                    int ny = y + deltaY[d];
+                    if (!isIn(nx, ny) || !board[nx][ny] || visited[nx][ny] > -1) continue;
+                    visited[nx][ny] = visited[x][y] + 1;
+                    queue.offer(new Pos(nx, ny));
+                }
+            }
+            return minDist != MAX_DIST ? new int[]{minDist, minRow, minCol} : null;
+        }
+        boolean move() {
+            int[] passenger = findPassenger();
+            if (passenger == null) { // 이동 불가능
+                return false;
+            }
+            int dist = passenger[0];
+            int x = passenger[1];
+            int y = passenger[2];
+
+            // 승객의 출발지로 이동
+            setPos(x, y);
+            useFuel(dist);
+
+            // 승객의 목적지로 이동
+            if (!moveToDst()) { // 이동 불가능
+                return false;
+            }
+            return true;
+        }
+        boolean moveToDst() {
+            // 승객을 목적지까지 이동시키는데 사용한 연료의 두 배 충전
+            int dist = 0;
+            int sx = pos.x; // 출발 위치
+            int sy = pos.y;
+            int key = N * sx + sy;
+            Pos dst = passengers.get(key);
+            int dx = dst.x; // 도착 위치
+            int dy = dst.y;
+
+            Queue<Pos> queue = new ArrayDeque<>();
+            int[][] visited = new int[N + 1][N + 1]; // 이동 거리
+            for (int i = 1; i <= N; i++) {
+                Arrays.fill(visited[i], -1);
+            }
+            queue.offer(pos);
+            visited[sx][sy] = 0;
+
+            while (!queue.isEmpty()) {
+                Pos cur = queue.poll();
+                int x = cur.x;
+                int y = cur.y;
+                if (visited[x][y] > fuel) { // 이동 불가능
+                    return false;
+                }
+                if (x == dx && y == dy) { // 도착
+                    dist = visited[x][y];
+                    break;
+                }
+                for (int d = 0; d < deltaX.length; d++) {
+                    int nx = x + deltaX[d];
+                    int ny = y + deltaY[d];
+                    if (!isIn(nx, ny) || !board[nx][ny] || visited[nx][ny] > -1) continue;
+                    queue.offer(new Pos(nx, ny));
+                    visited[nx][ny] = visited[x][y] + 1;
+                }
+            }
+            if (dist == 0) { // 이동 불가능
+                return false;
+            }
+            useFuel(dist);
+            chargeFuel(dist);
+            passengers.remove(key); // 승객 삭제
+            numPassenger -= 1;
+            setPos(dx, dy);
+            return true;
+        }
+    }
+    static int N, M, numPassenger;
+    static final int MAX_DIST = 40, MAX_ROW = 20, MAX_COL = 20;
+    static boolean[][] board; // true: 빈 칸, false: 벽
+
+    static int[] deltaX = {-1, 0, 1, 0};
+    static int[] deltaY = {0, 1, 0, -1};
+    static Map<Integer, Pos> passengers = new HashMap<>(); // key: 출발 위치의 1차원 좌표, value: 도착 위치
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        int fuel = Integer.parseInt(st.nextToken());
+        board = new boolean[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                board[i][j] = !st.nextToken().equals("1");
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        Taxi taxi = new Taxi(x, y, fuel);
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int sx = Integer.parseInt(st.nextToken());
+            int sy = Integer.parseInt(st.nextToken());
+            int dx = Integer.parseInt(st.nextToken());
+            int dy = Integer.parseInt(st.nextToken());
+            int key = N * sx + sy;
+            passengers.put(key, new Pos(dx, dy));
+        }
+        numPassenger = passengers.size();
+
+        while (numPassenger > 0) {
+            if (!taxi.move()) { // 실패
+                System.out.println(-1);
+                return;
+            }
+        }
+        System.out.println(taxi.fuel);
+    }
+
+    static boolean isIn(int x, int y) {
+        return x >= 1 && x <= N && y >= 1 && y <= N;
+    }
+}
