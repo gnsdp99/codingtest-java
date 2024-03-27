8c260ecebb1042b47878c68ed7afa3a5e728f1a3
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2024-03-27 10:43:34 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 84 ms, Memory: 11776 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] LIS = new int[N];
+        int size = 0;
+        LIS[size++] = A[0];
+        for (int i = 1; i < N; i++) {
+            if (A[i] > LIS[size - 1]) {
+                LIS[size++] = A[i];
+            } else {
+                int target = lowerBound(LIS, size, A[i]);
+                LIS[target] = A[i];
+            }
+        }
+        System.out.println(size);
+    }
+
+    static int lowerBound(int[] LIS, int size, int cur) {
+        int left = 0, right = size - 1;
+        int target = -1;
+        while (left <= right) {
+
+            int mid = left + ((right - left) >> 1);
+            
+            if (LIS[mid] >= cur) {
+                // 오른쪽은 절대 정답이 아님
+                target = mid;
+                right = mid - 1;
+            } else {
+                // 왼쪽은 절대 정답이 아님
+                left = mid + 1;
+            }
+        }
+        return target;
+    }
+}
