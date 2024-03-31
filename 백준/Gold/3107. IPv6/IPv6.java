b76f5ad3e906891b3914b9c06fa4e2867f7349ca
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2024-03-31 16:43:58 +0900

    [Gold V] Title: IPv6, Time: 136 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Gold/3107. IPv6/IPv6.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] ipv6 = br.readLine().split(":");
+        int len = ipv6.length;
+
+        int cnt = 0;
+        int start = -1;
+        int next = -1;
+        for (int i = 0; i < len; i++) {
+            if (!ipv6[i].isEmpty()) {
+                cnt++;
+                if (ipv6[i].length() < 4) {
+                    StringBuilder zero = new StringBuilder();
+                    for (int j = 0; j < 4 - ipv6[i].length(); j++) {
+                        zero.append("0");
+                    }
+                    ipv6[i] = zero.append(ipv6[i]).toString();
+                }
+                if (start != -1 && next == -1) {
+                    next = i;
+                }
+            } else {
+                if (start == -1) {
+                    start = i;
+                }
+            }
+        }
+
+        if (start == -1) {
+            start = cnt;
+        }
+
+        if (next == -1) {
+            next = 8;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        int i = 0;
+        for (;i < start; i++) {
+            sb.append(ipv6[i]).append(":");
+        }
+        for (int j = 0; j < 8 - cnt; j++) {
+            sb.append("0000:");
+        }
+        for (i = next; i < len; i++) {
+            sb.append(ipv6[i]).append(":");
+        }
+        if (sb.charAt(sb.length() - 1) == ':') {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        System.out.println(sb);
+    }
+}
