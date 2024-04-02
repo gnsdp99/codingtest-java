73738cf2af538dcd1508e94087fe84e0dbea2ee6
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2024-04-02 14:25:01 +0900

    [Silver III] Title: 파도반 수열, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        long[] arr = new long[101];
+        arr[1] = arr[2] = arr[3] = 1;
+        arr[4] = arr[5] = 2;
+        for (int i = 6; i <= 100; i++) {
+            arr[i] = arr[i - 1] + arr[i - 5];
+        }
+
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(arr[N]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
