bfc62bb14ad59910a308334c697a2b96bb52d5a2
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2024-04-04 21:33:18 +0900

    [Gold IV] Title: 플로이드, Time: 432 ms, Memory: 43532 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,37 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static class Node {
-        int num, dist;
-        Node next;
-
-        Node(int num, int dist, Node next) {
-            this.num = num;
-            this.dist = dist;
-            this.next = next;
-        }
-    }
-
-    static class Pair implements Comparable<Pair> {
-        int num;
-        long dist;
-
-        Pair(int num, long dist) {
-            this.num = num;
-            this.dist = dist;
-        }
-
-        @Override
-        public int compareTo(Pair pair) {
-            return Long.compare(dist, pair.dist);
-        }
-    }
 
     public static void main(String[] args) throws IOException {
 
@@ -41,46 +13,38 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        Node[] adjList = new Node[N + 1];
+        int INF = 100000 * 100;
+
+        int[][] dists = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i != j) {
+                    dists[i][j] = INF;
+                }
+            }
+        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
             int C = Integer.parseInt(st.nextToken());
-
-            adjList[A] = new Node(B, C, adjList[A]);
+            dists[A][B] = dists[A][B] > C ? C : dists[A][B];
         }
 
-        long INF = 100000L * 100000;
-        boolean[] visited = new boolean[N + 1];
-        long[] dists = new long[N + 1];
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        for (int start = 1; start <= N; start++) {
-            Arrays.fill(visited, false);
-            Arrays.fill(dists, INF);
-            dists[start] = 0;
-            pq.offer(new Pair(start, 0));
-
-            while (!pq.isEmpty()) {
-
-                Pair pair = pq.poll();
-
-                if (dists[pair.num] < pair.dist) {
-                    continue;
-                }
-
-                visited[pair.num] = true;
-
-                for (Node adj = adjList[pair.num]; adj != null; adj = adj.next) {
-                    if (!visited[adj.num] && dists[adj.num] > pair.dist + adj.dist) {
-                        dists[adj.num] = pair.dist + adj.dist;
-                        pq.offer(new Pair(adj.num, dists[adj.num]));
+        for (int mid = 1; mid <= N; mid++) {
+            for (int start = 1; start <= N; start++) {
+                for (int end = 1; end <= N; end++) {
+                    if (dists[start][end] > dists[start][mid] + dists[mid][end]) {
+                        dists[start][end] = dists[start][mid] + dists[mid][end];
                     }
                 }
             }
-            for (int node = 1; node <= N; node++) {
-                sb.append(dists[node] < INF ? dists[node] : 0).append(" ");
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                sb.append(dists[i][j] < INF ? dists[i][j] : 0).append(" ");
             }
             sb.append("\n");
         }
