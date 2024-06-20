08f3a8faef7a9b47cc28506bf16901e167b5c2ae
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2024-06-20 17:09:39 +0900

    [Silver IV] Title: 수 찾기, Time: 548 ms, Memory: 42816 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -4,7 +4,7 @@ import java.util.*;
 class Main {
 
     static int N, M;
-    static Set<Integer> set = new HashSet<>();
+    static int[] arr;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,16 +12,33 @@ class Main {
         StringBuilder sb = new StringBuilder();
 
         N = Integer.parseInt(br.readLine());
+        arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        while (st.hasMoreTokens()) {
-            set.add(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(arr);
         M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         while (st.hasMoreTokens()) {
             int num = Integer.parseInt(st.nextToken());
-            sb.append(set.contains(num) ? 1 : 0).append("\n");
+            sb.append(isExist(num) ? 1 : 0).append("\n");
         }
         System.out.println(sb);
+    }
+
+    static boolean isExist(int num) {
+        int l = 0, r = N - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (arr[m] < num) {
+                l = m + 1;
+            } else if (arr[m] > num) {
+                r = m - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 }
