07073f01a007be3f43c9911499487590ffc925f7
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2024-07-20 01:47:15 +0900

    [Gold II] Title: 친구 네트워크, Time: 1040 ms, Memory: 65840 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
@@ -2,97 +2,67 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
-
-/**
- * @author 김예훈
- * @date 24/02/21
- * @link https://www.acmicpc.net/problem/4195
- * @keyword_solution
- * - 두 집합의 크기를 구해야 하는 유니온 파인드 문제.
- * - 두 사람이 같은 집합에 속해 있으면 현재 크기, 아니면 합한 크기를 구한다.
- * - 매번 집합의 크기를 구하려면 N^2이 된다.
- * - 따라서 연산할 때마다 크기를 기록해야 한다.
- * - 이름을 숫자로 표시하기 위해 Map을 이용한다.
- *
- * @input
- * - 친구 관계의 수 F [1, 100,000]
- *
- * @output
- * - 친구 관계가 생길 때마다 두 사람의 친구 네트워크에 몇 명이 있는지 출력.
- *
- * @time_complex
- * - O(E)
- *
- * @perf
- */
+import java.util.TreeMap;
 
 public class Main {
-    static class Subset {
-        int parent, rank;
-
-        Subset(int parent, int rank) {
-            this.parent = parent;
-            this.rank = rank;
-        }
-    }
 
     static int T, F;
-    static Map<String, Integer> strToInt;
-    static Subset[] subsets;
+    static Map<String, Integer> stringIntegerMap;
+    static int[] parent;
+    static int[] rank;
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
-        subsets = new Subset[200_001];
         T = Integer.parseInt(br.readLine());
-        for (int tc = 0; tc < T; tc++) {
+        for (int t = 0; t < T; t++) {
+            stringIntegerMap = new TreeMap<>();
             F = Integer.parseInt(br.readLine());
-            strToInt = new HashMap<>();
+            parent = new int[(F << 1) + 1];
+            rank = new int[(F << 1) + 1];
+            Arrays.fill(rank, 1);
+            for (int i = 0; i < parent.length; i++) {
+                parent[i] = i;
+            }
+            int count = 1;
             for (int i = 0; i < F; i++) {
                 st = new StringTokenizer(br.readLine());
-                String name1 = st.nextToken();
-                String name2 = st.nextToken();
-                makeSet(name1);
-                makeSet(name2);
-
-                int res = union(strToInt.get(name1), strToInt.get(name2));
-                sb.append(res).append("\n");
+                String A = st.nextToken();
+                String B = st.nextToken();
+                if (!stringIntegerMap.containsKey(A)) {
+                    stringIntegerMap.put(A, count++);
+                }
+                if (!stringIntegerMap.containsKey(B)) {
+                    stringIntegerMap.put(B, count++);
+                }
+                union(stringIntegerMap.get(A), stringIntegerMap.get(B));
+                sb.append(rank[parent[stringIntegerMap.get(A)]]).append("\n");
             }
         }
         System.out.println(sb);
     }
 
-    static void makeSet(String name) {
-        if (!strToInt.containsKey(name)) {
-            strToInt.put(name, strToInt.size());
-            int num = strToInt.get(name);
-            subsets[num] = new Subset(num, 1);
+    static int find(int a) {
+        if (parent[a] == a) {
+            return a;
         }
+        return parent[a] = find(parent[a]);
     }
 
-    static int findSet(int v) {
-        if (subsets[v].parent == v) return v;
-        return subsets[v].parent = findSet(subsets[v].parent);
-    }
+    static void union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
 
-    static int union(int v1, int v2) {
-        int p1 = findSet(v1);
-        int p2 = findSet(v2);
-        if (p1 == p2) return subsets[p1].rank;
-
-        if (subsets[p1].rank >= subsets[p2].rank) {
-            subsets[p2].parent = p1;
-            subsets[p1].rank += subsets[p2].rank;
-            return subsets[p1].rank;
-        } else {
-            subsets[p1].parent = p2;
-            subsets[p2].rank += subsets[p1].rank;
-            return subsets[p2].rank;
+        if (pa == pb) {
+            return;
         }
+
+        parent[pb] = pa;
+        rank[pa] += rank[pb];
     }
 }
