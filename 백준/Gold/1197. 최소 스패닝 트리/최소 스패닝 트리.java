4d844f15fc3cd48ee2f62d1043704c37252c6127
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2024-10-05 20:34:33 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 476 ms, Memory: 48736 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,39 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+import java.util.PriorityQueue;
 
-public class Main { // Kruskal Algorithm
+public class Main {
 
     static class Edge implements Comparable<Edge> {
-        int src, dst, weight;
+        int u, v, w;
 
-        Edge(int src, int dst, int weight) {
-            this.src = src;
-            this.dst = dst;
-            this.weight = weight;
+        Edge(int u, int v, int w) {
+            this.u = u;
+            this.v = v;
+            this.w = w;
         }
 
-        public int compareTo(Edge e) {
-            return Integer.compare(weight, e.weight);
-        }
-    }
-
-    static class Subset {
-        int parent, rank;
-
-        Subset(int parent, int rank) {
-            this.parent = parent;
-            this.rank = rank;
+        @Override
+        public int compareTo(Edge edge) {
+            return Integer.compare(w, edge.w);
         }
     }
+
     static int V, E;
-    static long ans = 0;
-    static PriorityQueue<Edge> edgesPQ = new PriorityQueue<>();
-    static Subset[] subsets; // 부분 집합 배열
+    static int[] parent;
+    static int[] rank;
+    static PriorityQueue<Edge> pq = new PriorityQueue();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -42,45 +33,62 @@ public class Main { // Kruskal Algorithm
         st = new StringTokenizer(br.readLine());
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
-        subsets = new Subset[V + 1];
+
+        parent = new int[V + 1];
+        rank = new int[V + 1];
         for (int i = 1; i <= V; i++) {
-            subsets[i] = new Subset(i, 0); // Make-set 연산
+            parent[i] = i;
         }
+
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
             int C = Integer.parseInt(st.nextToken());
-            edgesPQ.offer(new Edge(A, B, C));
+            pq.offer(new Edge(A, B, C));
         }
 
-        kruskal();
+        int ans = connect();
         System.out.println(ans);
     }
 
-    static void kruskal() {
-        int numSelected = 0; // MST 집합에 포함한 간선의 수
-        while (numSelected < V - 1) {
-            Edge edge = edgesPQ.poll();
-            int rootSrc = find(edge.src);
-            int rootDst = find(edge.dst);
-            if (rootSrc != rootDst) { // 사이클을 생성하지 않으므로 합집합
-                union(rootSrc, rootDst);
-                ans += edge.weight;
-                numSelected++;
+    static int connect() {
+        int sum = 0;
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
+            int A = edge.u;
+            int B = edge.v;
+            if (union(A, B)) {
+                sum += edge.w;
             }
         }
+        return sum;
     }
 
-    static int find(int v) {
-        int parent = subsets[v].parent;
-        if (parent == v) return v;
-        return subsets[v].parent = find(parent); // 경로 압축
+    static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        }
+        return parent[a] = find(parent[a]);
     }
 
-    static void union(int root1, int root2) {
-        if (subsets[root1].rank == subsets[root2].rank) subsets[root1].rank++;
-        if (subsets[root1].rank > subsets[root2].rank) subsets[root2].parent = root1;
-        else subsets[root1].parent = root2;
+    static boolean union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+
+        if (pa == pb) {
+            return false;
+        }
+
+        if (rank[pa] < rank[pb]) {
+            parent[pa] = pb;
+        } else {
+            parent[pb] = pa;
+
+            if (rank[pa] == rank[pb]) {
+                ++rank[pa];
+            }
+        }
+        return true;
     }
 }
