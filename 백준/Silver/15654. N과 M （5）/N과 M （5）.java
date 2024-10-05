e94c6b3b7986d39385565e252b395148dab9c14a
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2024-10-05 14:43:35 +0900

    [Silver III] Title: N과 M (5), Time: 312 ms, Memory: 28788 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+
+    static int N, M;
+    static int[] numbers;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        numbers = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numbers);
+
+        makePermutation(new int[M], new boolean[N], 0);
+
+        System.out.println(sb);
+    }
+
+    static void makePermutation(int[] selected, boolean[] isSelected, int idx) {
+        if (idx >= M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(selected[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (isSelected[i]) {
+                continue;
+            }
+            selected[idx] = numbers[i];
+            isSelected[i] = true;
+            makePermutation(selected, isSelected, idx + 1);
+            isSelected[i] = false;
+        }
+    }
+}
