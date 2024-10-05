051e6ceaab95e782f5edf1d32e0e3f245bf92208
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2024-10-05 19:39:14 +0900

    [Gold IV] Title: 최단경로, Time: 588 ms, Memory: 103684 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,92 +1,95 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
-
-/**
- * @author 김예훈
- * @date 24/02/27
- * @link https://www.acmicpc.net/problem/1753
- * @keyword_solution  
- * - 시작점에서 다른 모든 정점으로의 최단 경로를 구하는 것이고 가중치가 양수이므로 다익스트라 알고리즘 사용
- * 
- * @input 
- * - 정점의 개수 V [1, 20,000]
- * - 간선의 개수 E [1, 300,000]
- * - 가중치 [1, 10]
- * 
- * @output
- * - 각 정점으로의 최단 경로값 출력.
- * - 시작점은 0으로 출력, 경로가 존재하지 않으면 INF 출력
- * 
- * @time_complex  O(E * logV)
- * @perf 
- */
+import java.util.PriorityQueue;
+import java.util.Arrays;
 
 public class Main {
-	
-	static class Node implements Comparable<Node> {
-		int num, weight;
-		Node(int dst, int weight) {
-			this.num = dst;
-			this.weight = weight;
-		}
-		public int compareTo(Node o) {
-			return Integer.compare(weight, o.weight);
-		}
-	}
 
-	static int V, E, K;
-	static final int INF = Integer.MAX_VALUE;
-	static int[] minDist;
-	static ArrayList<ArrayList<Node>> adjList = new ArrayList<>();
-	static PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		minDist = new int[V + 1];
-		Arrays.fill(minDist, INF);
-		
-		for (int i = 0; i <= V; i++) {
-			adjList.add(new ArrayList<>());
-		}
-		
-		K = Integer.parseInt(br.readLine());
-		for (int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			adjList.get(u).add(new Node(v, w));
-		}
-		
-		minDist[K] = 0; // 시작점
-		priorityQueue.offer(new Node(K, 0));
-		while (!priorityQueue.isEmpty()) {
-			Node cur = priorityQueue.poll();
-			
-			if (minDist[cur.num] < cur.weight) continue;
-			
-			for (Node adj: adjList.get(cur.num)) {
-				if (minDist[adj.num] > cur.weight + adj.weight) {
-					minDist[adj.num] = cur.weight + adj.weight;
-					priorityQueue.offer(new Node(adj.num, minDist[adj.num]));
-				}
-			}
-		}
-		
-		for (int i = 1; i <= V; i++) {
-			sb.append(minDist[i] != INF ? minDist[i] : "INF").append("\n");
-		}
-		System.out.println(sb);
-	}
+    static class Node {
+        int num, weight;
+        Node next;
+
+        Node(int num, int weight, Node next) {
+            this.num = num;
+            this.weight = weight;
+            this.next = next;
+        }
+    }
+
+    static class Pair implements Comparable<Pair> {
+        int num, dist;
+
+        Pair(int num, int dist) {
+            this.num = num;
+            this.dist = dist;
+        }
+
+        @Override
+        public int compareTo(Pair p) {
+            return Integer.compare(dist, p.dist);
+        }
+    }
+
+    static int V, E, K;
+    static int[] dist;
+    static Node[] adjList;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(br.readLine());
+
+        dist = new int[V + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[K] = 0;
+
+        adjList = new Node[V + 1];
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            adjList[u] = new Node(v, w, adjList[u]);
+        }
+
+        find(K);
+
+        for (int i = 1; i <= V; i++) {
+            sb.append(dist[i] != Integer.MAX_VALUE ? dist[i] : "INF").append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void find(int src) {
+        boolean[] visited = new boolean[V + 1];
+        visited[src] = true;
+
+        PriorityQueue<Pair> pq = new PriorityQueue();
+        pq.offer(new Pair(src, 0));
+
+        while (!pq.isEmpty()) {
+            Pair p = pq.poll();
+            if (p.dist > dist[p.num]) {
+                continue;
+            }
+
+            visited[p.num] = true;
+
+            for (Node adj = adjList[p.num]; adj != null; adj = adj.next) {
+                int sum = p.dist + adj.weight;
+                if (dist[adj.num] > sum) {
+                    dist[adj.num] = sum;
+                    pq.offer(new Pair(adj.num, dist[adj.num]));
+                }
+            }
+        }
+    }
 }
