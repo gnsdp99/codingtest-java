23d19034dd165daaa5678df16dd2966073d8e1db
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2024-10-05 14:20:52 +0900

    [Silver III] Title: N과 M (1), Time: 240 ms, Memory: 21960 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,70 +3,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- * @author 김예훈
- * @date 24/01/30
- * @link https://www.acmicpc.net/problem/15649
- * @keyword_solution
- * - 1부터 N까지의 자연수 중에서 "중복 없이" M개를 고른 수열을 모두 구한다.
- * - 길이 M의 중복 없는 수열 -> 순열
- * 
- * - 재귀로 순열 구하기
- * - 함수 정의: 숫자를 하나 선택한다.
- * - 변경 요소: 선택한 숫자 배열, 숫자를 선택할 자릿수
- * - 기저 조건: 모든 자리에 숫자를 선택했을 때 종료
- *   
- * @input 
- * - 1 <= M <= N <= 8 >> 최대 8!이므로 완탐 가능.
- * 
- * @output
- * - 한 줄에 하나의 수열을 "사전 순으로" 출력한다. >> 1 ~ N으로 이루어진 배열을 오름차순으로 선언한다.
- *
- * @time_complex O(N!)
- * @perf 
- */
-
 public class Main {
-	
-	static int N, M;
-	static int[] numbers;
-	static StringBuilder sb;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		numbers = new int[N];
-		for (int i = 0; i < N; i++) {
-			numbers[i] = i + 1;
-		}
-		
-		// logic
-		makePermutation(new int[M], 0, new boolean[N]);
-		System.out.println(sb);
-	}
-	
-	static void makePermutation(int[] selected, int curIdx, boolean[] isSelected) {
-		// curIdx: selected 배열의 숫자를 선택할 위치
-		if (curIdx >= M) {
-			for (int num: selected) {
-				sb.append(num).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		for (int i = 0; i < N; i++) {
-			if (!isSelected[i]) {
-				selected[curIdx] = numbers[i];				
-				isSelected[i] = true;
-				makePermutation(selected, curIdx + 1, isSelected);
-				isSelected[i] = false;
-			}
-		}
-	}
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        makePermutation(new int[M], new boolean[N + 1], 0);
+        System.out.println(sb);
+    }
+
+    static void makePermutation(int[] perm, boolean[] selected, int idx) {
+        if (idx >= M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(perm[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (selected[i]) {
+                continue;
+            }
+            selected[i] = true;
+            perm[idx] = i;
+            makePermutation(perm, selected, idx + 1);
+            perm[idx] = 0;
+            selected[i] = false;
+        }
+    }
 }
